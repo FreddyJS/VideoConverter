40de1151c42c2773da32d8c70d658f04816c3c21
Freddy <33833131+FreddyJS@users.noreply.github.com>
2021-07-09 05:19:46 +0200

    Added imgui lib

---
+++ README.md
@@ -0,0 +1,3 @@
+cmake --build build --config Debug
+cmake --build build --config Release
+cmake --build build --target PACKAGE --config Release
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello CMake with ImGUI\n";
+    std::string msg;
+
+    std::cin >> msg;
+
+    return 0;
+}
